975206792b3ecf8ae93a403b8ca02920d04cdd14
User <swathi.vidiyala@gmail.com>
2021-06-17 21:53:46 +0530

    new changes

--- Testng/src/test/java/TestNG/LoginGmail.java
+++ Testng/src/test/java/TestNG/LoginGmail.java
@@ -2,6 +2,9 @@ package TestNG;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -9,9 +12,9 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class LoginGmail {
 @Test
 	public void loginmail() {
-		WebDriverManager.edgedriver().setup();
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\User\\Desktop\\Selenium softwares\\chromedriver_win32\\chromedriver.exe");
 		ChromeDriver driver=new ChromeDriver();
-		driver.get("https://www.google.com/");
+		driver.get("https://www.google.com");
 		driver.findElement(By.name("q")).sendKeys("javatpoint");
 		driver.findElement(By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[3]/center/input[1]")).click();
 
